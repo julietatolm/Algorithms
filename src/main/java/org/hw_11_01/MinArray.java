d853ad3451445aa95a6dd7a75eade95cab1714a1
Yulia_Tolmacheva <julietatolm@gmail.com>
2022-11-07 21:51:09 +0100

    update homework

--- src/main/java/org/hw_11_01/MinArray.java
+++ src/main/java/org/hw_11_01/MinArray.java
@@ -4,18 +4,23 @@ public class MinArray {
     //Написать метод который находит 3 и 4 ый элемент в массиве
     public static void main(String[] args) {
         int[] array = {15, 3, 684, 6, 41, 32, 1, 5, 46, 86, 43, 12, 11};
+//        int[] s = {99, 99, 99, 99, 99, 99};
+//        int[] d = {1, 99, 99, 99, 99};
+//        int[] a = {99, 99, 99, 99, 1};
 
         getThirdAndFourthMin(array);
+
+//        getThirdAndFourthMin(a);
 
     }
 
     public static void getThirdAndFourthMin(int[] array) {
-        int min1 = array[0];
+        int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
         int min3 = Integer.MAX_VALUE;
         int min4 = Integer.MAX_VALUE;
 
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] < min1) {
                 min4 = min3;
                 min3 = min2;
